4743fe8ed7df460343c4f02aa289542d4d1dbe86
bcyu <chanyb0827@gmail.com>
2021-12-15 09:39:17 +0900

    init

---
+++ app/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Home"/>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="info" @tap="vibrate()">
+                <FormattedString>
+                    <Span class="fas" text.decode="&#xf135; "/>
+                    <Span :text="message" />
+                </FormattedString>
+            </Label>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+import { Application } from '@nativescript/core';
+export default {
+    computed: {
+        message() {
+            return "Blank {N}-Vue";
+        }
+    },
+    methods: {
+        vibrate: function() {
+            console.log("clicked1");
+            //android.content.Context.VIBRATOR_SERVICE
+            let vibrateService = Application.android.context.getSystemService(android.content.Context.VIBRATOR_SERVICE);
+            vibrateService.vibrate(2000);
+            // console.log(Object.keys(vibrateService));
+            console.log("clicked2");
+        },
+    }
+};
+</script>
+
+<style scoped lang="scss">
+    @import '@nativescript/theme/scss/variables/blue';
+
+    // Custom styles
+    .fas {
+        @include colorize($color: accent);
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
