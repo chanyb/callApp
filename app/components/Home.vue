14ec0c93a2aa79a8d76d5f7451ddfe302bf0a0b4
swjeong <swjeong@jbcpistui-iMac.local>
2021-12-26 19:31:28 +0900

    iOS push notification 성공

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -180,12 +180,24 @@ export default {
             firebase.init({
                 // Optionally pass in properties for database, authentication and cloud messaging,
                 // see their respective docs.
+                showNotifications: true,
+                showNotificationsWhenInForeground: true,
+                iOSEmulatorFlush: true,
+                onMessageReceivedCallback: function(message) {
+                    console.log("Title: " + message.title);
+                    console.log("Body: " + message.body);
+                    // if your server passed a custom property called 'foo', then do this:
+                    console.log("Value of 'foo': " + message.data.foo);
+                },
+                onPushTokenReceivedCallback: function(token) {
+                    console.log("Firebase push token: " + token);
+                },
             }).then(
                 () => {
-                    console.log("firebase.init 12ddone");
+                    console.log("firebase.init done");
                 },
                 error => {
-                    console.log(`firebase.init 12doneerror: ${error}`);
+                    console.log(`firebase.init error: ${error}`);
                 }
             );
         },
